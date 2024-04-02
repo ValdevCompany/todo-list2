9e1d76f4c70380ac43e206be2639239ec7e025e5
ValdevCompany <141391313+ValdevCompany@users.noreply.github.com>
2024-04-02 20:34:40 +0600

    Add files via upload

--- Projects/todo.py
+++ Projects/todo.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template,request,make_response
+from flask import Flask, render_template, request, make_response, redirect
 import os
-app = Flask(__name__,template_folder = 'C:/Users/User/Desktop/Projects/htm code')
- 
+
+app = Flask(__name__, template_folder='C:/Users/User/Desktop/Projects/html code')
+
 TASKS_FILE = 'tasks.txt'
+
+
 def load_tasks():
     if os.path.exists(TASKS_FILE):
         with open(TASKS_FILE, 'r') as f:
@@ -10,29 +13,45 @@ def load_tasks():
     else:
         return []
 
-# Функция для сохранения задач в файл
+
 def save_tasks(tasks):
     with open(TASKS_FILE, 'w') as f:
         for task in tasks:
             f.write(task + '\n')
 
-tasks = load_tasks()
+
+def delete_task(index):
+    tasks = load_tasks()
+    if 0 <= index < len(tasks):
+        del tasks[index]
+        save_tasks(tasks)
+        return True
+    else:
+        return False
 
 
-
-@app.route('/',methods = ['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def main():
+    tasks = load_tasks()
     if request.method == 'POST':
-
         task = request.form['task']
-
-        response = make_response(render_template('todo.html', tasks=tasks))
-        response.set_cookie('task', task)
         if task:
             tasks.append(task)
             save_tasks(tasks)
-    return render_template('todo.html',tasks = tasks)
+            # Redirect to avoid form resubmission
+            return redirect('/')
+    return render_template('todo.html', tasks=tasks)
 
 
-if __name__ == __name__:
-    app.run(debug = True,port = '5050')
+@app.route('/delete_task', methods=['POST'])
+def delete_task_route():
+    index = int(request.form['index'])
+    if delete_task(index):
+        # Redirect to avoid form resubmission
+        return redirect('/')
+    else:
+        return 'Failed to delete task'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
